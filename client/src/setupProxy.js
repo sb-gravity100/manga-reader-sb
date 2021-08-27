f6ce7c9dda00c2e50bf4c892aea166f9f5406370
Sb <sbducao0@gmail.com>
2021-08-27 22:48:43 +0000

    Done

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = app => {
-   app.use('/api', createProxyMiddleware({
-      target: 'http://localhost:7800',
-      changeOrigin: true,
-   }))
-}
+module.exports = (app) => {
+   app.use(
+      '/api',
+      createProxyMiddleware({
+         target: 'http://localhost:7800',
+         changeOrigin: true,
+      })
+   );
+   app.use(
+      '/cdn',
+      createProxyMiddleware({
+         target: 'http://localhost:7800',
+         changeOrigin: true,
+      })
+   );
+};
